d057e603cc8ff668523ede4109c089c922fae285
Aurelien Naldi <aurelien.naldi@gmail.com>
2021-01-07 17:12:28 +0100

    Find prime implicants without converting to NNF first

--- src/func/expr2primes.rs
+++ src/func/expr2primes.rs
@@ -5,15 +5,21 @@ impl Expr {
     /// Compute the prime implicants of a Boolean expression.
     pub fn prime_implicants(&self) -> Implicants {
         let mut paths = Implicants::new();
-        self.nnf()
-            .as_ref()
-            .unwrap_or(self)
-            ._prime_implicants(&mut paths);
+        self._prime_implicants_pure(&mut paths);
         paths
     }
 
-    /// Look for prime implicants, dissolving dlinks as we go
-    fn _prime_implicants(&self, paths: &mut Implicants) {
+    /// Dispatch prime implicant construction to specialized functions depending on the negated status
+    fn _prime_implicants(&self, paths: &mut Implicants, neg: bool) {
+        if neg {
+            self._prime_implicants_neg(paths);
+        } else {
+            self._prime_implicants_pure(paths);
+        }
+    }
+
+    /// Look for prime implicants, dissolving dlinks as we go, for non-negated expressions
+    fn _prime_implicants_pure(&self, paths: &mut Implicants) {
         if paths.is_empty() {
             return;
         }
@@ -23,34 +29,57 @@ impl Expr {
             Expr::FALSE => paths.clear(),
             Expr::ATOM(u) => paths.extend_literal(*u, true),
             Expr::NATOM(u) => paths.extend_literal(*u, false),
-            Expr::OPER(Operator::OR, c) => {
-                let n = c.len();
-                if n < 1 {
-                    // An empty disjunction is false
-                    Expr::FALSE._prime_implicants(paths);
-                    return;
-                }
-
-                if n == 1 {
-                    c.data[0]._prime_implicants(paths);
-                    return;
-                }
-
-                let mut source = paths.clone();
-                c.data[0]._prime_implicants(paths);
-                for i in 1..n {
-                    source.substract(paths);
-                    let mut next = source.clone();
-                    c.data[i]._prime_implicants(&mut next);
-                    paths.merge_raw(&next);
-                }
-            }
-            Expr::OPER(Operator::AND, c) => {
-                for ch in c.data.iter() {
-                    ch._prime_implicants(paths);
-                }
-            }
-            _ => panic!("Input formula is not in NNF: {}", self),
+            Expr::OPER(Operator::OR, c) => Expr::_pi_or(c, paths, false),
+            Expr::OPER(Operator::NOR, c) => Expr::_pi_and(c, paths, true),
+            Expr::OPER(Operator::AND, c) => Expr::_pi_and(c, paths, false),
+            Expr::OPER(Operator::NAND, c) => Expr::_pi_or(c, paths, true),
         };
+    }
+
+    /// Look for prime implicants, dissolving dlinks as we go, for negated expressions
+    fn _prime_implicants_neg(&self, paths: &mut Implicants) {
+        if paths.is_empty() {
+            return;
+        }
+
+        match self {
+            Expr::TRUE => paths.clear(),
+            Expr::FALSE => (),
+            Expr::ATOM(u) => paths.extend_literal(*u, false),
+            Expr::NATOM(u) => paths.extend_literal(*u, true),
+            Expr::OPER(Operator::OR, c) => Expr::_pi_and(c, paths, true),
+            Expr::OPER(Operator::NOR, c) => Expr::_pi_or(c, paths, false),
+            Expr::OPER(Operator::AND, c) => Expr::_pi_or(c, paths, true),
+            Expr::OPER(Operator::NAND, c) => Expr::_pi_and(c, paths, false),
+        };
+    }
+
+    fn _pi_and(c: &Children, paths: &mut Implicants, neg: bool) {
+        for ch in c.data.iter() {
+            ch._prime_implicants(paths, neg);
+        }
+    }
+
+    fn _pi_or(c: &Children, paths: &mut Implicants, neg: bool) {
+        let n = c.len();
+        if n < 1 {
+            // An empty disjunction is false
+            Expr::FALSE._prime_implicants(paths, neg);
+            return;
+        }
+
+        if n == 1 {
+            c.data[0]._prime_implicants(paths, neg);
+            return;
+        }
+
+        let mut source = paths.clone();
+        c.data[0]._prime_implicants(paths, neg);
+        for i in 1..n {
+            source.substract(paths);
+            let mut next = source.clone();
+            c.data[i]._prime_implicants(&mut next, neg);
+            paths.merge_raw(&next);
+        }
     }
 }
